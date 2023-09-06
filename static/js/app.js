60812baeb78ecf8f4afe33eb16b10475d746f1d5
Damian1255 <damianlau96@gmail.com>
2023-09-06 16:30:06 +0800

    wip

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,3 @@
-var input = document.getElementById("input");
-var go = document.getElementById("go");
-var p = document.querySelector("p");
-
-input.addEventListener("keyup", function () {
-    if (input.value.length >= 2) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/search', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                if (response.success) {
-                    p.innerHTML = "";
-                    for (row of response.data) {
-                        p.innerHTML += "ID: " + row.NDB_No + " Desc: " + row.Descrip + "<br>";
-                    }
-                } else {
-                    alert("Search failed.");
-                }
-            } else {
-                alert('An Internal error occurred.');
-            }
-        };
-
-        xhr.send(JSON.stringify({
-            search_query: input.value
-        }));
-    }
-})
-
 //Parallax Scrolling
 window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
