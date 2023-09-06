2aa40fbb8ae151bb855ed9c8c20d71cf5b242ab8
Your Name <you@example.com>
2023-09-06 16:21:33 +0800

    temporary updates

--- static/js/app.js
+++ static/js/app.js
@@ -29,3 +29,13 @@ input.addEventListener("keyup", function () {
         }));
     }
 })
+
+//Parallax Scrolling
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
